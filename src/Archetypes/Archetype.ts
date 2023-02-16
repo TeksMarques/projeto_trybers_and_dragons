f3d9c16ba68e683812e44a1af58ddfc9e6aeac77
TeksMarques <taotaodoce@gmail.com>
2023-02-16 18:16:23 -0300

    requisito 4 concluido

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,31 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _special: number;
+  private _cost: number;
+
+  constructor(private _name: string) {
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+
+  static createdArchetypeInstances(): void {
+    throw new Error('Not implemented');
+  }
+}
+
+export default Archetype;
